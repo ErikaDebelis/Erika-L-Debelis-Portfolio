d60c445410ecccbfa542c307792e06e04bdde199
Erika Debelis <erika.debelis@gmail.com>
2021-10-04 21:24:46 -0700

    reworking imgs routes- fix + update base path

--- next.config.js
+++ next.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  basePath: '/Erika-L-Debelis-Portfolio',
+  basePath: '/',
 }
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'MAZE',
     description:" A full-stack C# ASP .Net Core API back-end and Javascript/React.js front-end. A User will be able to navigate the maze, choose-your-own-adventure style in a dynamic web application. Will you survive the maze? Can you solve the riddle?",
-    image: '/images/Maze.png',
+    image: '../images/Maze.png',
     tags: ['ASP Net Core', ',' , 'EF Core', ',' , 'MySQL', ',' , 'React', ',' , 'Webpack'],
     source: 'https://github.com/ErikaDebelis/MAZE',
     visit: 'https://github.com/ErikaDebelis/MAZE',
@@ -11,7 +11,7 @@ export const projects = [
   {
     title: 'Tap Room',
     description: "A React-built web application which allows a user to catalog kegs with full CRUD functionality. A user can see how many pints are left in the keg (a full keg holds 124 pints). A user can click a button to sell a pint (pints cannot not go below 0).",
-      image: '/images/Tap-Room.png',
+      image: '../images/Tap-Room.png',
       tags: ['CSS', ',' , 'JSX', ',' , 'React', ',' , 'Bootstrap'],
     source: 'https://github.com/ErikaDebelis/react-tap-room',
     visit: 'https://github.com/ErikaDebelis/react-tap-room',
@@ -20,7 +20,7 @@ export const projects = [
   {
     title: 'Animal Shelter API',
     description: "A C# ASP .Net Core API code repository for a hypothetical animal shelter. API has 3 models with distinct configurations and the database is populated through use of migrations to MySQL. API features pagination, CORS, and a Swagger Swashbuckle UI",
-      image: '/images/Animal-Shelter.png',
+      image: '../images/Animal-Shelter.png',
       tags: ['C#', ',' , 'EF Core', ',' , 'ASP NET Core', ',' , 'Swagger', ',' , 'Postman'],
     source: 'https://github.com/ErikaDebelis/AnimalShelterAPI.Solution',
     visit: 'https://github.com/ErikaDebelis/AnimalShelterAPI.Solution',
@@ -29,7 +29,7 @@ export const projects = [
   {
     title: 'Hair Salon',
     description: "A C# ASP .Net Core code repository for a hypothetical hair salon. The user can assign stylists and add corresponding clients; demonstrating a one-to-many relationship. This app was built with MySql + MySQL Workbench and EntityFramework Core. ",
-    image: '/images/Hair-Salon.png',
+    image: '../images/Hair-Salon.png',
     tags: ['C#', ',' , 'EF Core', ',' , 'ASP NET Core', ',' , 'Bootstrap'],
     source: 'https://github.com/ErikaDebelis/HairSalon.Solution',
     visit: 'https://github.com/ErikaDebelis/HairSalon.Solution',
