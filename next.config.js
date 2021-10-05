816a692b9c91c09fc1e6e421b03a08ffce34f0b5
Erika Debelis <erika.debelis@gmail.com>
2021-10-04 21:40:24 -0700

    WIP:attempt again to fix next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   exportPathMap: function () {
     return {
-      "/": { page: "/" },
+      "/": { page: "/Erika-L-Debelis-Portfolio/" },
       "/ap-grid-layout": { page: "/ap-grid-layout" },
       "/ap-highlight": { page: "/ap-highlight" },
     }
